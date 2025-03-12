433540a1928341b142b7b9573c11b419c1a60ecd
Djheyson Oliveira <djheyson.dev@gmail.com>
2025-03-12 14:10:24 -0300

    src/patterns/binarySearch/isSameTree

---
+++ src/patterns/binarySearch/isSameTree/isSameTree.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from "../TreeNode";
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+export const isSameTree = function (p: TreeNode, q: TreeNode): boolean {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
